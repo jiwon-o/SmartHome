8d610f71a5835d9c79847e3575f4dc395faf0bbe
오지원 <64193469+ojw4184@users.noreply.github.com>
2021-02-10 10:52:05 +0900

    Bottom navigation bar 구현

--- app/src/main/java/com/appforest/smarthome/MainActivity.java
+++ app/src/main/java/com/appforest/smarthome/MainActivity.java
@@ -1,14 +1,66 @@
 package com.appforest.smarthome;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mBottomNV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Navigation Listener
+        mBottomNV = findViewById(R.id.bottom_navigation_view);
+        mBottomNV.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                BottomNavigate(menuItem.getItemId());
+
+                return true;
+            }
+        });
+        mBottomNV.setSelectedItemId(R.id.action_home);
+    }
+
+    //Bottom navigation function
+    private void BottomNavigate(int id) {
+        String tag = String.valueOf(id);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        Fragment currentFragment = fragmentManager.getPrimaryNavigationFragment();
+        if (currentFragment != null) {
+            fragmentTransaction.hide(currentFragment);
+        }
+
+        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+        if (fragment == null) {
+            if (id == R.id.action_home) {
+                fragment = new HomeFragment();
+            } else if (id == R.id.action_stats) {
+                fragment = new StatsFragment();
+            } else {
+                fragment = new SettingFragment();
+            }
+            fragmentTransaction.add(R.id.frame_layout, fragment, tag);
+        } else {
+            fragmentTransaction.show(fragment);
+        }
+
+        fragmentTransaction.setPrimaryNavigationFragment(fragment);
+        fragmentTransaction.setReorderingAllowed(true);
+        fragmentTransaction.commitNow();
+
     }
 }
