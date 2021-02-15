69e843ed74f7fc128e1199af02a071be1bddd10d
오지원 <64193469+ojw4184@users.noreply.github.com>
2021-02-15 11:15:08 +0900

    StatsFragment에 하단 드로어 생성(Umano/AndroidSlidingUpPanel)

--- app/src/main/java/com/appforest/smarthome/StatsFragment.java
+++ app/src/main/java/com/appforest/smarthome/StatsFragment.java
@@ -7,6 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +27,7 @@ public class StatsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
 
     public StatsFragment() {
         // Required empty public constructor
@@ -59,6 +64,20 @@ public class StatsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stats, container, false);
+        View view = inflater.inflate(R.layout.fragment_stats, container, false);
+
+        final ListView listView = view.findViewById(R.id.listView);
+        listView.setAdapter(new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1,
+                new String[]{"TV", "A/C", "Speaker"}));
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(), position+" 번째 값 : " + parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
+
 }
